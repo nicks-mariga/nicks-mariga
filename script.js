a95978ee862930cb61d67ec1600af614d78dd731
nicks-mariga <nicks0mariga@gmail.com>
2024-10-11 16:08:42 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,4 @@
+// JavaScript to handle the button click event
+document.getElementById('messageButton').addEventListener('click', function() {
+    document.getElementById('message').innerText = 'NOT ronaldo';
+});
